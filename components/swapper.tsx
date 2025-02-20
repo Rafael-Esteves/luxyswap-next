3695e2b8bf7076a2695cabf3d3c5c7fc3aef8899
jovimoura <joaovictors.mouraa@gmail.com>
2025-02-19 22:17:07 -0300

    fix 1

--- components/swapper.tsx
+++ components/swapper.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { ArrowUpDown, ChevronDown } from "lucide-react";
-import { motion } from "framer-motion";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Button, buttonVariants } from "./ui/button";
+import Link from "next/link";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { Coin } from "@/types/Coin";
 import coinMetadata from '@/public/coin-icons/metadata.json';
+import { useDynamicFontSize } from "@/hooks/use-dynamic-font-size";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Input } from "./ui/input";
 
 export function Swapper() {
   const [fromValue, setFromValue] = useState("0.00");
@@ -30,6 +34,9 @@ export function Swapper() {
   const [coinIcons, setCoinIcons] = useState<{
     [key: string]: { content: string; isSvg: boolean };
   }>({});
+
+  const fromFontSize = useDynamicFontSize(fromValue);
+  const toFontSize = useDynamicFontSize(toValue);
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -115,11 +122,14 @@ export function Swapper() {
           <input
             value={fromValue}
             onChange={(e) => setFromValue(e.target.value)}
-            className="font-bold w-[30%] bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 font-gravesend text-xl lg:text-5xl tracking-tighter select-none text-white"
+            className={cn(
+              "font-bold w-[30%] bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 font-gravesend tracking-tighter select-none text-white transition-all duration-200",
+              `text-xl lg:text-${fromFontSize}`
+            )}
           />
         </div>
       </div>
-      <div className="p-5 rounded-full bg-[#D9D9D9]/15 backdrop-blur-lg absolute top-[35%] z-10">
+      <div className="p-5 rounded-full bg-[#D9D9D9]/15 backdrop-blur-lg absolute top-[40%] z-10">
         <ArrowUpDown className="text-white" />
       </div>
 
@@ -158,11 +168,62 @@ export function Swapper() {
             <input
               value={toValue}
               onChange={(e) => setToValue(e.target.value)}
-              className="font-bold w-[30%] bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 font-gravesend text-xl lg:text-5xl tracking-tighter select-none text-white"
+              className={cn(
+                "font-bold w-[30%] bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 font-gravesend tracking-tighter select-none text-white transition-all duration-200",
+                `text-xl lg:text-${toFontSize}`
+              )}
             />
           </div>
         </div>
       </div>
+      {openAddress && (
+          <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: openAddress ? 1 : 0, y: openAddress ? 0 : 50 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+          className="flex flex-col gap-2 items-end justify-end w-full"
+        >
+            <span className="font-medium text-sm font-gravesend mr-10">
+              TAXAS: 5%
+            </span>
+            <div className="bg-[#D9D9D94D] rounded-full w-full p-2 flex items-center justify-between">
+              <Input
+                placeholder="YOUR ETH ADRESS"
+                className="font-medium bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 font-scandia uppercase text-base tracking-tighter select-none text-white"
+              />
+
+              <Link href='/swap'>
+                <Image
+                  src="/icons/arrow-btn-purple.svg"
+                  className="cursor-pointer"
+                  alt=""
+                  width={87}
+                  height={49}
+                />
+              </Link>
+            </div>
+            </motion.div>
+        )}
+      {!openAddress && (
+        <motion.div
+        initial={{ opacity: 1, y: 0 }}
+        animate={{ opacity: openAddress ? 0 : 1, y: openAddress ? 50 : 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        className="w-full"
+      >
+        <Button
+          onClick={() => setOpenAddress(true)}
+          className={cn(
+            buttonVariants({ variant: "outline" }),
+            "bg-transparent rounded-full h-16 w-2/3 mt-2"
+          )}
+        >
+          <span className="text-xl lg:text-2xl font-gravesend font-bold">
+            SWAP
+          </span>
+        </Button>
+        </motion.div>
+      )}
     </div>
   );
 }
